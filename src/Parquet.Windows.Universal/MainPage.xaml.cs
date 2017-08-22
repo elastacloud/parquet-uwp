6bfd38c7fa447fe61eaa5068545f534e7ef05795
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-22 13:50:23 +0100

    save work

--- src/Parquet.Windows.Universal/MainPage.xaml.cs
+++ src/Parquet.Windows.Universal/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,14 +18,14 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Parquet.Windows.Universal
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+   /// <summary>
+   /// An empty page that can be used on its own or navigated to within a Frame.
+   /// </summary>
+   public sealed partial class MainPage : Page
+   {
+      public MainPage()
+      {
+         this.InitializeComponent();
+      }
+   }
 }
