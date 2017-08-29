52220c6b2a018980fcd998b8665419157a355ca5
Richard Conway <richard@elastacloud.com>
2017-08-29 14:05:13 +0100

    changes to mainframe

--- src/Parquet.Windows.Universal/MainPage.xaml.cs
+++ src/Parquet.Windows.Universal/MainPage.xaml.cs
@@ -1,27 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Parquet.Data;
 using Parquet.Windows.Universal.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Parquet.Windows.Universal.Controls;
-using Syncfusion.UI.Xaml.Controls.Input;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
